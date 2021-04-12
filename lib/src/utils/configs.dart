9aaab37fc4f0113d85d828862e966be4838183f4
MO-SALAH-Daia <mohammad.daia@gmail.com>
2021-04-12 10:31:26 +0200

    first commit

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,79 @@
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+// const String APP_ID = "476";
+// const String AUTH_KEY = "PDZjPBzAO8WPfCp";
+// const String AUTH_SECRET = "6247kjxXCLRaua6";
+// const String ACCOUNT_ID = "TpuBZox_HPxofh7PVZdP";
+// const String DEFAULT_PASS = "xxasBUM3gQs36bhj";
+
+const String APP_ID = "4484";
+const String AUTH_KEY = "LWY8zyD4MReYve6";
+const String AUTH_SECRET = "XJ5ZEDDYwCMXeNs";
+const String ACCOUNT_ID = "S82q8Hp1ST67sk7WQCGg";
+const String DEFAULT_PASS = "12345678";
+
+List<CubeUser> users = [
+  CubeUser(
+    id: 3863936,
+    login: "user1",
+    fullName: "User11",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 3863941,
+    login: "user2",
+    fullName: "User22",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 3871724,
+    login: "user3",
+    fullName: "User33",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 3871725,
+    login: "user4",
+    fullName: "User44",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 3983358,
+    login: "hossam16177836880",
+    fullName: "hossam16177836880",
+    password: 'hossam1617783688560',
+  ),
+  CubeUser(
+    id: 3983364,
+    login: "Salah1617793398",
+    fullName: "Salah1617793398",
+    password: 'Salah161779339842',
+  )
+];
+
+// List<CubeUser> users = [
+//   CubeUser(
+//     id: 1253158,
+//     login: "call_user_1",
+//     fullName: "User 1",
+//     password: DEFAULT_PASS,
+//   ),
+//   CubeUser(
+//     id: 1253159,
+//     login: "call_user_2",
+//     fullName: "User 2",
+//     password: DEFAULT_PASS,
+//   ),
+//   CubeUser(
+//     id: 1253160,
+//     login: "call_user_3",
+//     fullName: "User 3",
+//     password: DEFAULT_PASS,
+//   ),
+//   CubeUser(
+//     id: 1253162,
+//     login: "call_user_4",
+//     fullName: "User 4",
+//     password: DEFAULT_PASS,
+//   )
+// ];
